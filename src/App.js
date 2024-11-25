299fbc744c0d9d3f637390466cd188bf516f8b6c
Rustemov02 <bunyamin2002@mail.ru>
2024-11-26 02:20:31 +0400

    t end

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import React, { useEffect, useState, lazy, Suspense } from "react";
+import React, { useEffect, useState } from "react";
 import "./reset.css";
 import "./index.css";
 
-// import { Header } from './Header/Header'
 import Nav from "./MainSection/Nav";
-// import Categories from './Categories'
 import OrderNavigation from "./OrderNavigation";
-// import BestSeller from './BestSeller'
 import Register from "./Register";
 import Services from "./Services";
 import { useDispatch } from "react-redux";
@@ -20,35 +17,36 @@ function App() {
   const [isLoaded, setIsLoaded] = useState(false);
 
   useEffect(() => {
-    dispatch(fillData()); 
-
+    dispatch(fillData());
 
     setTimeout(() => {
       setIsLoaded(true);
-    }, 1000); 
+    }, 2000);
   }, [isLoaded]);
 
   return (
     <>
-      {isLoaded ? (
-        <div className="relative"> 
-          <Bar />
-          <Nav />
-          <OrderNavigation /> 
-          <Services />
-          <Register />
-          <Footer />
-        </div>
-      ) : (
-        <div className="flex justify-center items-center h-screen">
-          <Mosaic
-            color="#c3922f"
-            size="large"
-            text="Loading..."
-            textColor="#c79827"
-          />
-        </div>
-      )}
+      <div
+        className={`flex justify-center items-center h-screen ${
+          isLoaded ? "hidden" : ""
+        } `}
+      >
+        <Mosaic
+          color="#c3922f"
+          size="large"
+          text="Loading..."
+          textColor="#c79827"
+        />
+      </div>
+
+      <div className={`relative ${isLoaded ? "" : "hidden"}`}>
+        <Bar />
+        <Nav />
+        <OrderNavigation />
+        <Services />
+        <Register />
+        <Footer />
+      </div>
     </>
   );
 }
